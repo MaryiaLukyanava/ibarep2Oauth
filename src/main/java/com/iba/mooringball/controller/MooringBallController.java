f7a46e77328e875988e465f256293cd7749d4bca
Sawko_AS <lexsawko@gmail.com>
2018-03-06 18:22:28 +0300

    swagger implemented

--- src/main/java/com/iba/mooringball/controller/MooringBallController.java
+++ src/main/java/com/iba/mooringball/controller/MooringBallController.java
@@ -3,26 +3,38 @@ package com.iba.mooringball.controller;
 import com.iba.mooringball.entity.MooringBall;
 import com.iba.mooringball.resourceSupport.MooringBallResource;
 import com.iba.mooringball.service.MooringBallService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resources;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 
 @RestController
 @RequestMapping(value = "mooringballs", produces = "application/hal+json")
+@Api(value="mooringballs", description="Operations pertaining to mooring balls")
 public class MooringBallController {
     @Autowired
     MooringBallService service;
 
     @GetMapping
+    @ApiOperation(value = "View a list of available mooring balls", response = Resources.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
     public Resources<MooringBallResource> getMooringBalls(){
         List<MooringBallResource> list = service.getMooringBalls()
                 .stream()
@@ -35,17 +47,21 @@ public class MooringBallController {
     }
 
     @GetMapping(value = "/{id}")
+    @ApiOperation(value = "Search a mooring ball with an ID",response = MooringBallResource.class)
     public MooringBallResource getMooringBall(@PathVariable Long id){
         return new MooringBallResource(service.getMooringBallById(id));
     }
 
     @PostMapping
+    @ApiOperation(value = "Add a mooring ball",response = MooringBallResource.class)
     public MooringBallResource addMooringBall(@RequestBody MooringBall ball){
         service.addMooringBall(ball);
         return new MooringBallResource(service.getMooringBallById(ball.getBallId()));
     }
 
     @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Update a mooring ball by ID",response = MooringBallResource.class)
     public MooringBallResource updateMooringBall(@RequestBody MooringBall ball, @PathVariable Long id){
        ball.setBallId(id);
        service.updateMooringBall(ball);
@@ -53,6 +69,7 @@ public class MooringBallController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @ApiOperation(value = "Delete a product")
     public void deleteBooringBallById(@PathVariable Long id){
         service.deleteMooringBall(id);
     }
--- src/main/java/com/iba/mooringball/entity/MooringBall.java
+++ src/main/java/com/iba/mooringball/entity/MooringBall.java
@@ -2,10 +2,10 @@ package com.iba.mooringball.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.hateoas.ResourceSupport;
 
 import javax.persistence.*;
 
@@ -20,10 +20,13 @@ public class MooringBall{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @ApiModelProperty(notes = "The database generated mooring ball's ID")
     private Long ballId;
 
     @Column(name = "latitude", nullable = false)
+    @ApiModelProperty(notes = "Mooring ball's latitude")
     private double latitude;
     @Column(name = "longitude", nullable = false)
+    @ApiModelProperty(notes = "Mooring ball's longitude")
     private double longitude;
 }
